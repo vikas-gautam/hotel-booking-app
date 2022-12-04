6115918506063fa9ca2fe0cc6d0f63056304dc04
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-12-04 13:57:49 +0530

    render html files

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+const portNumber = ":9090"
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	log.Printf("Starting server on port: %s", portNumber)
+	http.ListenAndServe(portNumber, nil)
+
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.html")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.html")
+}
+
+func renderTemplate(w http.ResponseWriter, fileName string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + fileName)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		log.Println("error parsing html templates", err)
+	}
+}
