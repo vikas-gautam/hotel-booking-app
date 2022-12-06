7a2d47f9254250e1a5f354ff5beda2fb08a87eff
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-12-06 10:17:43 +0530

    adding cache templates bcz template parsing is reading runtime changes as well

--- main.go
+++ main.go
@@ -1,35 +1,20 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/vikas-gautam/hotel-booking-app/pkg/handlers"
 )
 
-const portNumber = ":9090"
+const portNumber = ":7070"
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	log.Printf("Starting server on port: %s", portNumber)
 	http.ListenAndServe(portNumber, nil)
 
-}
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
-}
-
-func renderTemplate(w http.ResponseWriter, fileName string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + fileName)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		log.Println("error parsing html templates", err)
-	}
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,16 +1,59 @@
 package render
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-func RenderTemplate(w http.ResponseWriter, fileName string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+fileName, "./templates/base.layout.html")
-	err := parsedTemplate.Execute(w, nil)
+// func RenderTemplate(w http.ResponseWriter, fileName string) {
+// 	parsedTemplate, _ := template.ParseFiles("./templates/"+fileName, "./templates/base.layout.html")
+// 	err := parsedTemplate.Execute(w, nil)
+// 	if err != nil {
+// 		log.Println("error parsing html templates", err)
+// 		return
+// 	}
+// }
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+
+	var template *template.Template
+	var err error
+
+	_, inMap := tc[t]
+	if !inMap {
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		log.Println("using cached templates")
+	}
+
+	template = tc[t]
+	err = template.Execute(w, nil)
 	if err != nil {
-		log.Println("error parsing html templates", err)
+		log.Println(err)
 		return
 	}
+
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.html",
+	}
+
+	temp, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+
+	tc[t] = temp
+
+	return nil
 }
